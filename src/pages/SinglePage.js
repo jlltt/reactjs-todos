c06031473c80bc742f7871804a9dcece892e4339
Tommy Tang <tommyclone@gmail.com>
2021-03-11 14:26:22 +0800

    Test CI trigger

--- src/pages/SinglePage.js
+++ src/pages/SinglePage.js
@@ -5,7 +5,7 @@ const SinglePage = () => {
     const aboutData = [
         {
             slug: "about-app",
-            title: "About the App",
+            title: "About the App (modified)",
             description:
                 "In this app, you can add, delete, submit and edit items. To edit items, simply double click on it. Once you are done, press the enter key to resubmit. This app will persist your data in the browser local storage. So whether you reload, close your app or reopened it, you still have access to your to-dos items.",
         },
